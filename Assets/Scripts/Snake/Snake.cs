02c74eb5ab66b6e6084389d29528cf5447e476b6
Alexsei Kostiunin <alexsey.kostiunin@yandex.ru>
2021-11-13 14:24:43 +0300

    Init2

--- Assets/Scripts/Snake/Snake.cs
+++ Assets/Scripts/Snake/Snake.cs
@@ -5,18 +5,14 @@ public class Snake : MonoBehaviour
 {    
     [SerializeField] private MeshRenderer[] _snake;
 
-    private string NowNameControlPoint;
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.TryGetComponent(out PointChangeMaterial materialSetPoint))
         {
-            NowNameControlPoint = other.gameObject.name;
-
             foreach (var snake in _snake)
             {
                 snake.GetComponent<MeshRenderer>().material =
-                GameObject.Find(NowNameControlPoint).GetComponent<MeshRenderer>().material;
+                    other.gameObject.GetComponent<MeshRenderer>().material;
             }
         }
     }
